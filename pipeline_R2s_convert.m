aceaa4e0df2a405dcc71dd695d5f8368480b6eb8
mjt320 <36667519+mjt320@users.noreply.github.com>
2018-05-16 10:32:07 +0100

    Update
    Use dcm2niix instead of SPM
    opts.NEchoes no longer needed
    Some tidying.
    Generates 4D model.nii and signal.nii files, which contain data only for echoes specified in opts.fit.

--- pipeline_R2s_convert.m
+++ pipeline_R2s_convert.m
@@ -5,35 +5,28 @@ mkdir(opts.niftiDir); delete([opts.niftiDir '/*.*']);
 %% convert dicoms to 3D niftis
 dicomPaths=getMultipleFilePaths([opts.dicomDir '/*.dcm']);
 if isempty(dicomPaths); dicomPaths=getMultipleFilePaths([opts.dicomDir '/*.IMA']); end;
-
-MLB{1}.spm.util.dicom.data = dicomPaths;
-MLB{1}.spm.util.dicom.root = 'flat';
-MLB{1}.spm.util.dicom.outdir = {opts.niftiDir};
-MLB{1}.spm.util.dicom.convopts.format = 'nii';
-MLB{1}.spm.util.dicom.convopts.icedims = 0;
-spm_jobman('run', MLB);
+system(['dcm2niix -f magn_image_%e -o ' opts.niftiDir ' ' opts.dicomDir]);
 
 %% make a 4D nifti containing only magnitude data
 fileList={};
-for iEcho=1:opts.NEchoes
-    temp=sort(getMultipleFilePaths([opts.niftiDir '/*' num2str(iEcho,'%02d') '.nii'])); %get all files associated with this echo
-    echoImageFile=temp{1}; %assume first file is magnitude 
-    fileList=[fileList; echoImageFile];
+acqPars.NEchoes=size(dir([opts.niftiDir '/magn_image_*.nii']),1);
+for iEcho=1:acqPars.NEchoes
+    fileList=[fileList; opts.niftiDir '/magn_image_' num2str(iEcho) '.nii'];
 end
 spm_file_merge(fileList,[opts.niftiDir '/mag4D.nii'],0);
 
 copyfile(fileList{1},[opts.niftiDir '/firstEcho.nii']); % copy 3D magnitude image of first echo
-for iEcho=1:opts.NEchoes; delete([opts.niftiDir '/*-' num2str(iEcho,'%02d') '.nii']); end %delete 3D files
+delete([opts.niftiDir '/magn_image_*.nii']); %delete 3D files
 
 %% loop through dicoms and get echo times (dicominfo is surprisingly slow)
 allTE=[];
 while 1
     iDicom=floor(rand*size(dicomPaths,1))+1;
     temp=dicominfo(dicomPaths{iDicom}); allTE=[allTE; temp.EchoTime];
-    if size(unique(allTE),1)==opts.NEchoes; break; end
+    if size(unique(allTE),1)==acqPars.NEchoes; break; end
 end
 acqPars.TE=0.001*sort(unique(allTE)); %get unique echo times (s) and store in ascending order
-if size(acqPars.TE,1)~=opts.NEchoes; error('Number of echo times is not equal to expected number of echoes.'); end
+if size(acqPars.TE,1)~=acqPars.NEchoes; error('Number of echo times is not equal to expected number of echoes.'); end
 
 save([opts.niftiDir '/acqPars'],'acqPars');
 
